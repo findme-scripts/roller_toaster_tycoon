27b1cb1379ea187f3e897ccd265d65a897f043c5
findme-scripts <115198740+findme-scripts@users.noreply.github.com>
2022-10-08 11:35:24 -0500

    Update splines.lua

--- lua/autorun/splines.lua
+++ lua/autorun/splines.lua
@@ -4,20 +4,21 @@ if SERVER then return end
 Splines = 
 {
 	__call = function(self)
-		hook.Add("PostDrawOpaqueRenderables", "Source Spline - Render Context", function() self:RenderContext() end)
+		hook.Add("PostDrawOpaqueRenderables", "Splines - Render Context", function() self:RenderContext() end)
 	end,
 
 	__index = {
 		Active = {},
 		
-		New = function(self)
-			local tbl = {}
-			setmetatable(tbl, {__call = self.__SplineCall, __index = self.__SplineIndex})
+		New = function(self, controlpoints)
+			local spline = {}
+			setmetatable(spline, {__call = self.__SplineCall, __index = self.__SplineIndex})
+			spline(controlpoints)
 
-			table.insert(self.Active, tbl)
-			tbl.Key = #self.Active
+			table.insert(self.Active, spline)
+			spline.Key = #self.Active
 
-			return tbl
+			return spline
 		end,
 
 		Get = function(self, key)
@@ -37,18 +38,49 @@ Splines =
 
 
 
-	__SplineCall = function(self)
+	__SplineCall = function(self, controlpoints)
 		print("Setting up new spline.")
-		self.Start = Vector()
+		self.ControlPoints = controlpoints || {}
 	end,
 
 	__SplineIndex = {
-		Key = 0,
 
 		Render = function(self)
-			render.SetColorMaterial()
-			render.DrawSphere(self.Start, 2, 16, 16, color_white)
+			for i=1, #self.ControlPoints do
+				render.SetColorMaterial()
+				render.DrawSphere(self.ControlPoints[i], 2, 16, 16, color_white)
+			end
 		end
+
+		
+
+
+		--[[Add = function(self)
+			local Total_ControlPoints = self.NumCP+2
+			local Spacing = self.SP:Distance(self.EP)/(Total_ControlPoints-1)
+			local Direction = (self.EP-self.SP); Direction:Normalize();
+
+			for i=0, Total_ControlPoints-1 do --(i=0, -1) we steppin back.
+				table.insert(self.ControlPoints, self.SP+Direction*i*Spacing)
+			end
+		end
+
+		Calc_Spline = function(self)
+			local n = #self.ControlPoints-1
+			local t = self.t
+
+			local WeightedSum = Vector()
+			for i=0, n do --(Bernstein-Bezier Form)
+				local Derive = N_Factorial(n) / ( N_Factorial(i) * N_Factorial(n-i) )
+				if Derive == math.huge then Derive = 1 end
+
+				local weight = Derive * math.pow(t, i) * math.pow( 1-t, (n-i) )
+
+				WeightedSum = WeightedSum + (self.ControlPoints[i+1] * weight)
+			end
+					
+			self.SplinePos = WeightedSum
+		end--]]
 	}
 }
 
@@ -65,11 +97,8 @@ Splines()
 ------------TESTING------------
 
 for i=1, 12 do
-	local spline = Splines:New()
-	spline.Start = LocalPlayer():GetPos() + Vector(0, 0, math.random(4, 64))
+	local spline = Splines:New({LocalPlayer():GetPos()+Vector(0, 0, 20), LocalPlayer():GetPos()+Vector(0, 20, 20)})
+	PrintTable(spline)
 end
-
-local Random_Spline = Splines:GetAll()[ math.random( 1, #Splines:GetAll() ) ]
-print( Splines:Get( Random_Spline.Key ).Key )
 
 ----------END TESTING----------
