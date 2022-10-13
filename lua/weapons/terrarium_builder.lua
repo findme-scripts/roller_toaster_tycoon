72c59e8cd8587a2cb4111199dc29aec3bef6f69e
findme-scripts <115198740+findme-scripts@users.noreply.github.com>
2022-10-13 08:05:36 -0500

    Update terrarium_builder.lua

--- lua/weapons/terrarium_builder.lua
+++ lua/weapons/terrarium_builder.lua
@@ -43,6 +43,10 @@ function SWEP:InitializeVariables()
 	self.Variables[5] = nil --Middle Position
 	self.Variables[6] = nil --Mins
 	self.Variables[7] = nil --Maxs
+	self.Variables[8] = nil --Original Z Position
+	self.Variables[9] = nil --Last Eye Angles
+	self.Variables[10] = nil --Active Z Position
+	self.Variables[11] = nil --Last Z
 end
 
 function SWEP:Set(index, content)
@@ -53,25 +57,85 @@ function SWEP:Get(index)
 	return self.Variables[index]
 end
 
+function SWEP:IsControllingZ()
+	return self:GetOwner():KeyDown(IN_ATTACK2)
+end
+
+function SWEP:FinishedControllingZ()
+	return self:GetOwner():KeyReleased(IN_ATTACK2)
+end
+
+function SWEP:ZManager()
+
+	if !self:Get(8) then
+		self:Set(8, (self:Get(4) || self:Get(3))) --Original Z Position.
+		--Debug:Position("OriginalZ", self:Get(8), {{1, 16, 16, color_white}, {"DermaDefault", 15, 120, color_white}})
+	end
+
+
+	if !self:Get(9) then
+		self:Set(9, self:GetOwner():EyeAngles()) --Last Eye Angles
+	end
+
+	local Degree_Difference = (self:Get(9)-self:GetOwner():EyeAngles()).x
+	self:Set(9, self:GetOwner():EyeAngles()) --Last Eye Angles
+
+
+	if !self:Get(10) then
+		self:Set(10, self:Get(8)) --Active Z Position
+	end
+
+	local NewZ = self:Get(10).z + Degree_Difference*3
+	local New = Vector(self:Get(8).x, self:Get(8).y, math.Clamp(NewZ, self:Get(8).z, 100000))
+
+	self:Set(10, New)
+	--Debug:Position("ActiveZ", self:Get(10), {{1, 16, 16, color_white}, {"DermaDefault", 15, 135, color_white}})
+
+end
+
 function SWEP:RenderContext()
 	local CurrentStage = self:Get(1)
 
 	if CurrentStage == 1 then
-		self:Set(3, self:GetOwner():GetEyeTraceNoCursor().HitPos) --Active Position
-		Debug:Position("Active", self:Get(3), {{1, 16, 16, color_white}, {"DermaDefault", 15, 30, color_white}})
+		if !self:Get(3) then self:Set(3, self:GetOwner():GetEyeTraceNoCursor().HitPos) end --Set this cause IsControllingZ will not make start if right is held down too.
+
+		if !self:IsControllingZ() then
+			self:Set(3, self:GetOwner():GetEyeTraceNoCursor().HitPos) --Active Position
+			Debug:Position("Active", self:Get(3), {{1, 16, 16, color_white}, {"DermaDefault", 15, 30, color_white}})
+		end
 
 		self:CalculateLocals(self:Get(2), self:Get(3))
 		render.DrawWireframeBox( (self:Get(2)+self:Get(3))*0.5, Angle(0, 0, 0), self:Get(6), self:Get(7), color_white, false )
 	elseif CurrentStage == 2 then
 		render.DrawWireframeBox( self:Get(5), Angle(0, 0, 0), self:Get(6), self:Get(7), color_white, false )
+		self:CalculateLocals(self:Get(2), self:Get(4))
 	end
+
+	if CurrentStage == 1 || CurrentStage == 2 then
+		if self:FinishedControllingZ() then
+			self:Set(8, nil) --Removing Original Z Position
+			self:Set(9, nil) --Removing Original Eye Angles
+		end
+
+		if self:IsControllingZ() then
+			self:ZManager()
+		end
+	end
+
 end
 
 function SWEP:CalculateLocals(pos1, pos2)
+	if !self:Get(11) then self:Set(11, 0) end
+	if self:Get(10) then
+		if self:Get(8) then
+			self:Set(11, self:Get(10).z - self:Get(8).z )
+		end
+	end
+
 	local mid = (pos1-pos2)*0.5
 	local x, y, z = math.abs(mid.x), math.abs(mid.y), math.abs(mid.z)
 
-	self:Set(6, Vector(-x, -y, -z)) --Mins
+	self:Set(6, Vector(-x, -y, -z+self:Get(11))) --Mins
 	Debug:Position("Mins", mid + self:Get(6), {{1, 16, 16, color_black}, {"DermaDefault", 15, 85, color_white}})
 	self:Set(7, Vector(x, y, z)) --Maxs
 	Debug:Position("Maxs", mid + self:Get(7), {{1, 16, 16, color_black}, {"DermaDefault", 15, 100, color_white}})
